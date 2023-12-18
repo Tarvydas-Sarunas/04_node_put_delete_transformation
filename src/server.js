454d7ffa075be3d811edab6546a201851ba20729
SarunasT <sarunas.tarvydas@hotmail.com>
2023-12-18 08:10:31 +0100

    express cors nodemon morgan

--- src/server.js
+++ src/server.js
@@ -1,2 +1,15 @@
-"use strict";
-console.log("server.js file was loaded");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+// route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// listen
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
